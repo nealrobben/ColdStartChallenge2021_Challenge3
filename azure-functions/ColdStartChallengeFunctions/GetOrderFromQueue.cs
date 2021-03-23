8734d7c4cedbd5fe40392866cf796f4ef3c8583e
nealrobben <neal.robben@cegeka.com>
2021-03-23 17:45:16 +0100

    Added output for CosmosDB

--- azure-functions/ColdStartChallengeFunctions/GetOrderFromQueue.cs
+++ azure-functions/ColdStartChallengeFunctions/GetOrderFromQueue.cs
@@ -6,9 +6,34 @@ namespace ColdStartChallengeFunctions
     public static class GetOrderFromQueue
     {
         [FunctionName("GetOrderFromQueue")]
-        public static void Run([QueueTrigger("customer-orders", Connection = "AZURE_STORAGE_CONNECTIONSTRING")]string myQueueItem, ILogger log)
+        public static void Run([QueueTrigger("customer-orders", Connection = "AZURE_STORAGE_CONNECTIONSTRING")]string myQueueItem,
+            [CosmosDB(databaseName: "orderdatabase", collectionName: "orders", ConnectionStringSetting = "CosmosDBConnection")]out dynamic document, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            document = new
+            {
+                id = "",
+                user = "test",
+                date = "test",
+                icecream = new
+                {
+                    icecreamId = "",
+                    name = "",
+                    description = "",
+                    imageUrl = ""
+                },
+                status = "",
+                driver = new
+                {
+                    driverId = "null",
+                    name = "null",
+                    imageUrl = "null"
+                },
+                fullAddress = "",
+                deliveryPosition = "",
+                lastPosition = ""
+            };
         }
     }
 }
