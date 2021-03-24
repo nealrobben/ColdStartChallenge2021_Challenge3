f6638880cf5742fc3285dbbe361cb415e6b06adf
Neal Robben <neal.robben@gmail.com>
2021-03-24 23:21:47 +0100

    Working on updating documents

--- azure-functions/ColdStartChallengeFunctions/UpdateOrderStatus.cs
+++ azure-functions/ColdStartChallengeFunctions/UpdateOrderStatus.cs
@@ -7,9 +7,25 @@ namespace ColdStartChallengeFunctions
     public static class UpdateOrderStatus
     {
         [FunctionName("UpdateOrderStatus")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer,
+            [CosmosDB(databaseName: "orderdatabase", collectionName: "orders", ConnectionStringSetting = "CosmosDBConnection", 
+            SqlQuery = "SELECT * FROM c WHERE c.status = 'Accepted'")] ICollector<CosmosDBItem> documents, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            //foreach(var item in documents)
+            //{
+
+            //}
         }
     }
+
+    public class CosmosDBItem
+    {
+        public string MyProperty { get; set; }
+    }
+
+    //https://docs.microsoft.com/en-us/previous-versions/sandbox/functions-recipes/cosmos-db?tabs=csharp#save-multiple-documents-to-a-collection
+    //https://stackoverflow.com/questions/63336415/how-to-query-cosmosdb-from-inside-azure-functions-method-instead-of-attribute
+    //https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-cosmosdb-v2-input?tabs=csharp
 }
