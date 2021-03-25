caec2e6c7bb1494cd1538bf3ea5cfea657a1efa5
Neal Robben <neal.robben@gmail.com>
2021-03-25 23:20:55 +0100

    Implemented Update function

--- azure-functions/ColdStartChallengeFunctions/UpdateOrderStatus.cs
+++ azure-functions/ColdStartChallengeFunctions/UpdateOrderStatus.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -6,26 +10,72 @@ namespace ColdStartChallengeFunctions
 {
     public static class UpdateOrderStatus
     {
+        private static readonly string databaseName = "orderdatabase";
+        private static readonly string collectionName = "orders";
+
+        private static readonly string endpointUrl = Environment.GetEnvironmentVariable("CosmosDBUri");
+        private static readonly string authorizationKey = Environment.GetEnvironmentVariable("CosmosDBKey");
+
         [FunctionName("UpdateOrderStatus")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer,
-            [CosmosDB(databaseName: "orderdatabase", collectionName: "orders", ConnectionStringSetting = "CosmosDBConnection", 
-            SqlQuery = "SELECT * FROM c WHERE c.status = 'Accepted'")] ICollector<CosmosDBItem> documents, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-            //foreach(var item in documents)
-            //{
+            using (CosmosClient cosmosClient = new CosmosClient(endpointUrl, authorizationKey))
+            {
+                Container container = cosmosClient.GetContainer(databaseName, collectionName);
 
-            //}
+                QueryDefinition query = new QueryDefinition("select * from orders s where s.status = 'Accepted' ");
+                var acceptedOrders = new List<Order>();
+
+                using (FeedIterator<Order> resultSet = container.GetItemQueryIterator<Order>(
+                    query,
+                    requestOptions: new QueryRequestOptions()
+                    {
+                        MaxItemCount = 1
+                    }))
+                {
+                    while (resultSet.HasMoreResults)
+                    {
+                        acceptedOrders.AddRange(resultSet.ReadNextAsync().Result);
+                    }
+                }
+
+                foreach(var order in acceptedOrders)
+                {
+                    order.status = "Ready";
+                    container.ReplaceItemAsync(order,order.id);
+                }
+            }
         }
     }
 
-    public class CosmosDBItem
+    public class Order
     {
-        public string MyProperty { get; set; }
+        public string id { get; set; }
+        public string user { get; set; }
+        public string date { get; set; }
+        public IceCream icecream { get; set; }
+        public string status { get; set; }
+        public Driver driver { get; set; }
+        public string fullAddress { get; set; }
+        public string deliveryPosition { get; set; }
+        public string lastPosition { get; set; }
     }
 
-    //https://docs.microsoft.com/en-us/previous-versions/sandbox/functions-recipes/cosmos-db?tabs=csharp#save-multiple-documents-to-a-collection
-    //https://stackoverflow.com/questions/63336415/how-to-query-cosmosdb-from-inside-azure-functions-method-instead-of-attribute
-    //https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-cosmosdb-v2-input?tabs=csharp
+    public class IceCream
+    {
+        public string icecreamId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string imageUrl { get; set; }
+
+    }
+
+    public class Driver
+    {
+        public string driverId { get; set; }
+        public string name { get; set; }
+        public string imageUrl { get; set; }
+    }
 }
